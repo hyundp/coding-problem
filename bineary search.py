d6c608d55ef1f02ce0d023f608e4ff3d0a67f82e
dyun <zkdlxkxks@nate.com>
2023-01-09 20:05:49 +0900

    add: bineary search

---
+++ bineary search.py
@@ -0,0 +1,50 @@
+def bs1(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    # find exact value
+    start = 0
+    end = len(nums)-1
+    while(start <= end):
+        mid = (start+end)//2
+        if(nums[mid]==target):
+            return mid
+        elif(nums[mid]>target):
+            end = mid-1
+        else:
+            start = mid+1
+    return -1
+
+def bs2(nums, target):
+    # find upper bound -> target에 해당하는 값이 nums에 여러개 있을 때
+    start = 0
+    end = len(nums)
+    while(start<end):
+        mid = (start+end)//2
+        if(nums[mid]<=target):
+            start = mid+1
+        else:
+            end = mid
+
+    if(start>0 and nums[start-1]==target):
+        return start-1
+    else:
+        return -1
+    
+def bs3(nums, target):
+    # find lower bound -> target에 해당하는 값이 nums에 여러개 있을 때
+    start = 0
+    end = len(nums)-1
+    while(start<end):
+        mid = (start+end)//2
+        if(nums[mid]>=target):
+            end = mid
+        else:
+            start = mid + 1
+
+    if start < len(nums) and nums[start]==target:
+        return start
+    else:
+        return -1
