afda4ad9ef3617cac8419a8e3a2d5062a5e0f6cd
dyun <zkdlxkxks@nate.com>
2023-01-20 00:39:52 +0900

    update: 01Matrix (진행중)

--- 01Matrix.py
+++ 01Matrix.py
@@ -65,4 +65,23 @@ def updateMatrix(self, mat):
 
 # 방법3
 # 1. dp를 활용한다.
-# 2. 
+import math
+
+def updateMatrix(self, mat):
+    m, n = len(mat), len(mat[0])
+
+    for r in range(m):
+        for c in range(n):
+            if mat[r][c] > 0:
+                top = mat[r - 1][c] if r > 0 else math.inf
+                left = mat[r][c - 1] if c > 0 else math.inf
+                mat[r][c] = min(top, left) + 1
+
+    for r in range(m - 1, -1, -1):
+        for c in range(n - 1, -1, -1):
+            if mat[r][c] > 0:
+                bottom = mat[r + 1][c] if r < m - 1 else math.inf
+                right = mat[r][c + 1] if c < n - 1 else math.inf
+                mat[r][c] = min(mat[r][c], bottom + 1, right + 1)
+
+    return mat
