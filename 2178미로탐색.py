0a2e68d5e7137268779772a29a7ba65b7f3d7f1b
dyun <zkdlxkxks@nate.com>
2023-04-25 21:58:40 +0900

    add: 그래프 탐색(진행중)

---
+++ 2178미로탐색.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+def main():
+    
+    count = 0
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    n,m = map(int, sys.stdin.readline().split())
+    
+    q = deque([])
+    
+    
+if __name__ == '__main__':
+    main()
