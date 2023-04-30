df61e3588471e6da7b3dc37e950ad6f04b423d8b
dyun <zkdlxkxks@nate.com>
2023-04-30 19:17:48 +0900

    update: heapq 제거 및 함수화

--- 2667단지번호붙이기.py
+++ 2667단지번호붙이기.py
@@ -1,5 +1,6 @@
 import sys
 import heapq
+from collections import deque
 
 # 반례
 # 5
@@ -26,52 +27,42 @@ def main():
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     
-    q = []
-    heapq.heappush(q, (0,(0,0)))
-    
-    visited = [(0,0)]
+    def bfs(x,y, cnt):
+        
+        if graph[x][y] == 0:
+            return 0
+        
+        graph[x][y] = 0
+        q = deque()
+        q.append((x,y))
+        
+        
+        while(q):
+            x, y = q.popleft()
+            cnt+=1
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if nx < 0 or nx > n-1 or ny < 0 or ny > n-1:
+                    continue
+                
+                if graph[nx][ny] == 1:
+                    q.append((nx,ny))
+                    graph[nx][ny] = 0
+                    
+        return cnt
+        
+
     
     house = []
-    parent = [[[] for _ in range(n)] for _ in range(n)] # 2차원 빈배열을 초기화하려면 이렇게 해야한다. 
-    # parent = [[0]*n for _ in range(n)] -> 이 방식은 2차원이지만 빈 배열이 아닐때 초기화 하는 방법이다.
-    cnt = 0
-    
-    while(q):
-        _, point = heapq.heappop(q)
-        x, y = point
-        
-        if graph[x][y] == 1:
-            if parent[x][y] == 1:
-                cnt += 1
-            else:
-                if cnt == 1:
-                    house.append(cnt)
-                else:
-                    cnt += 1
-        else:
-            if cnt != 0:
-                house.append(cnt)
-            cnt = 0
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if nx < 0 or nx > n-1 or ny < 0 or ny > n-1:
-                continue
-            
-            if (nx,ny) in visited:
-                continue
-            
-            if graph[nx][ny] == 1:
-                heapq.heappush(q, (0,(nx,ny)))
-                parent[nx][ny] = graph[x][y]
-            else:
-                heapq.heappush(q, (1,(nx,ny)))
-            visited.append((nx,ny))
-
-    if cnt != 0:
-        house.append(cnt)
+    for i in range(n):
+        for j in range(n):
+            result = bfs(i,j,0)
+            if result != 0:
+                house.append(result)
+                
     
     house.sort()
     print(len(house))
