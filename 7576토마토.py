7ef40e1fe646f9ff3c1bdcb7700cc02f0dea8495
dyun <zkdlxkxks@nate.com>
2023-05-03 23:12:25 +0900

    update: 그래프탐색
    시작부분만 따로 빼서 탐색 -> 시간초과

--- 7576토마토.py
+++ 7576토마토.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 flag = True
 
@@ -10,30 +11,37 @@ def main():
         graph.append(list(map(int, sys.stdin.readline().split())))
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
+    start = deque()
+    
+    for i in range(m):
+        for j in range(n):
+            if graph[j][i] == 1:
+                start.append((j,i))
     
     def spread(x,y,visited):
         global flag
-        if graph[y][x] == 1:
+        if graph[x][y] == 1:
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 
-                if nx < 0 or ny < 0 or nx > m-1 or ny > n-1:
+                if nx < 0 or ny < 0 or nx > n-1 or ny > m-1 or (nx,ny) in visited:
                     continue
                 
-                if graph[ny][nx] == 0:
-                    graph[ny][nx] = 1
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
                     flag = True
                     visited.append((nx,ny))
+                    start.append((nx,ny))
     
     cnt = -1
     while(flag):
         visited = []
-        flag = False        
-        for i in range(m):
-            for j in range(n):
-                if (i,j) not in visited:
-                    spread(i,j,visited)
+        flag = False
+        t = len(start)
+        for _ in range(t):
+            x,y = start.popleft()        
+            spread(x,y,visited)
         cnt+=1
         
     if any(0 in g for g in graph):
