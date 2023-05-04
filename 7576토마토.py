488616af1fc2106d104226f3f4501f98bc4f45a0
dyun <zkdlxkxks@nate.com>
2023-05-04 22:44:00 +0900

    update: 그래프 탐색
    visited 배열이 날마다 초기화 되지 않게 수정 -> 시간초과

--- 7576토마토.py
+++ 7576토마토.py
@@ -1,46 +1,44 @@
 import sys
 from collections import deque
 
-flag = True
-
 def main():
-    global flag
+
     m, n = map(int, sys.stdin.readline().split())
     graph = []
-    for _ in range(n):
-        graph.append(list(map(int, sys.stdin.readline().split())))
+    start = deque()
+    visited = []
+    for a in range(n):
+        temp = list(map(int, sys.stdin.readline().split()))
+        for b in range(m):
+            if temp[b] == 1:
+                start.append((a,b))
+                visited.append((a,b))
+        graph.append(temp)
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-    start = deque()
-    
-    for i in range(m):
-        for j in range(n):
-            if graph[j][i] == 1:
-                start.append((j,i))
     
     def spread(x,y,visited):
-        global flag
         if graph[x][y] == 1:
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 
-                if nx < 0 or ny < 0 or nx > n-1 or ny > m-1 or (nx,ny) in visited:
+                if nx < 0 or ny < 0 or nx > n-1 or ny > m-1:
                     continue
                 
+                if (nx,ny) in visited or graph[nx][ny] == -1:
+                    continue
+            
                 if graph[nx][ny] == 0:
                     graph[nx][ny] = 1
-                    flag = True
                     visited.append((nx,ny))
                     start.append((nx,ny))
     
     cnt = -1
-    while(flag):
-        visited = []
-        flag = False
+    while(start):
         t = len(start)
         for _ in range(t):
-            x,y = start.popleft()        
+            x,y = start.popleft()
             spread(x,y,visited)
         cnt+=1
         
