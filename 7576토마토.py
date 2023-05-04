05c2baf8008ef583c3eab5f4758bd747cb519cac
dyun <zkdlxkxks@nate.com>
2023-05-04 23:52:42 +0900

    update: 그래프 탐색 (해결)
    cnt를 위해 사용되던 for문을 줄이고 graph자체를 업데이트, 2차원배열의 max값을 위해 max(max()) 사용 하지 않고그래프 업데이트할때 max 값도 업데이트, bfs 이해를 통한 visited 삭제

--- 7576토마토.py
+++ 7576토마토.py
@@ -6,47 +6,40 @@ def main():
     m, n = map(int, sys.stdin.readline().split())
     graph = []
     start = deque()
-    visited = []
     for a in range(n):
         temp = list(map(int, sys.stdin.readline().split()))
         for b in range(m):
             if temp[b] == 1:
                 start.append((a,b))
-                visited.append((a,b))
         graph.append(temp)
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
+    result = -1
     
-    def spread(x,y,visited):
-        if graph[x][y] == 1:
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                
-                if nx < 0 or ny < 0 or nx > n-1 or ny > m-1:
-                    continue
-                
-                if (nx,ny) in visited or graph[nx][ny] == -1:
-                    continue
-            
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = 1
-                    visited.append((nx,ny))
-                    start.append((nx,ny))
-    
-    cnt = -1
     while(start):
-        t = len(start)
-        for _ in range(t):
-            x,y = start.popleft()
-            spread(x,y,visited)
-        cnt+=1
-        
-    if any(0 in g for g in graph):
-        print(-1)
-    else:
-        print(cnt)
+        x,y = start.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-    
+            if nx < 0 or ny < 0 or nx > n-1 or ny > m-1:
+                continue
+        
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y]+1
+                if result < graph[nx][ny]:
+                    result = graph[nx][ny]
+                start.append((nx,ny))
+                    
+    for g in graph:
+        if 0 in g:
+            print(-1)
+            return
+    if result > 0: # 2차원 배열에서 전체 최댓값을 찾는 방법은 max(max(graph))가 아니라 그냥 돌면서 max값을 찾는 것이다.
+        print(result-1)
+    else:
+        print(0)
+            
+
 if __name__=='__main__':
     main()
