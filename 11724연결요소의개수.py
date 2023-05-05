3eafb6e80ce689ce0a538d9ee81f77b0268f7553
dyun <zkdlxkxks@nate.com>
2023-05-05 20:21:54 +0900

    add: 그래프탐색(dfs)
    1. 재귀제한 설정 ( dfs할때 무조건 생각 )
    2. visited를 false로 배열을 만들어서 요소 찾을 때 그냥 인덱스 접근 가능하게 만듬 -> 시간초과는 해결됐지만 틀림
    3. start를 2개 만들 필요없이 a하나만 넣으면 자동으로 b는 서치하게 되므로 하나만으로 변경

---
+++ 11724연결요소의개수.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
+def main():
+    
+    n,m = map(int, sys.stdin.readline().split())
+    graph = [[]for _ in range(n+1)]
+    start = []
+    visited = [False]*(n+1)
+    cnt = 0
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+        start.append(a)
+        graph[a].append(b)
+        graph[b].append(a)
+    def dfs(x):
+        visited[x] = True
+        for g in graph[x]:
+            if visited[g] == False:
+                dfs(g)
+    for i in start:
+        if visited[i] == False:
+            dfs(i)
+            cnt+=1
+    print(cnt)
+    
+    
+if __name__=='__main__':
+    main()
