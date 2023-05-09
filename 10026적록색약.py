b703e7bb8a102dc8fd2aa9298d3f1bf54b6bea0c
dyun <zkdlxkxks@nate.com>
2023-05-09 12:41:46 +0900

    update: 그래프 탐색 (완료)
    1. heapq -> deque로 변경
    2. 영역 확인 방법으로 모든 그래프를 탐색하면서 bfs를 반복하고, 그래프를 수정해가는 것으로 변경 ( 2667 단지번호 붙이기와 유사 )

--- 10026적록색약.py
+++ 10026적록색약.py
@@ -1,6 +1,5 @@
 import sys
-import heapq
-# from queue import PriorityQueue
+from collections import deque
 import copy
 
 def main():
@@ -17,35 +16,28 @@ def main():
                 temp2[i] = 'R'
         graph2.append(temp2)
                 
-
-    # p = PriorityQueue()
-    # p.put((0,(0,0)))
-    normal = bfs(graph,n)
-    weakness = bfs(graph2,n)
+    result1 = 0
+    result2 = 0
+    for a in range(n):
+        for b in range(n):
+            result1 += bfs(a,b,graph,n)
+            result2 += bfs(a,b,graph2,n)
+            
     
-    print(normal, weakness)
+    print(result1, result2)
         
-def bfs(graph, n):
-    q=[]
-    heapq.heappush(q, (0,(0,0,0)))
+def bfs(x,y,graph, n):
+    if graph[x][y] == 'X':
+        return 0
+    simbol = graph[x][y]
+    q=deque()
+    q.append((x,y))
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-    visited = [[False]*n for _ in range(n)]
-    visited[0][0] = True
-    result = []
-    bx,by = 0,0
-    cnt = 0
+    graph[x][y] = 'X'
     while(q):
-        pri,(_,x,y) = heapq.heappop(q)
-        # pri,(x,y) = p.get()
-        # print(x,y,graph[x][y])
-        if graph[x][y] == graph[bx][by]:
-            cnt+=1
-        else:
-            result.append(cnt)
-            cnt = 1
+        x,y = q.popleft()
             
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -53,23 +45,10 @@ def bfs(graph, n):
             if nx < 0 or ny < 0 or nx > n-1 or ny > n-1:
                 continue
             
-            if visited[nx][ny] == True:
-                continue
-            
-            if graph[nx][ny] == graph[x][y]:
-                heapq.heappush(q, (0,(_+1,nx,ny)))
-                # p.put((0,(nx,ny)))
-            else:
-                heapq.heappush(q, (1,(_+1,nx,ny)))
-            visited[nx][ny] = True
-        bx, by = x,y
-        # print(q)
-            
-    if cnt != 0:
-        result.append(cnt)
-    # print(result)
-        
-    return (len(result))    
+            if graph[nx][ny] == simbol:
+                q.append((nx,ny))
+                graph[nx][ny] = 'X'
+    return 1
     
 if __name__=='__main__':
     main()
