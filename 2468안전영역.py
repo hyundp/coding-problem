f610f25b8ea8370b265b92ec602835368b5c1480
dyun <zkdlxkxks@nate.com>
2023-05-10 12:57:52 +0900

    add: 그래프 탐색 (진행중)
    bfs로 영역만들기
    반례 존재 -> 비는 연속형이니까 거의 오지 않아서 1만큼의 높이도 안될 수 있다고 가정해야한다.

---
+++ 2468안전영역.py
@@ -0,0 +1,58 @@
+import sys
+from collections import deque
+import copy
+
+def main():
+    n = int(sys.stdin.readline().rstrip())
+    graph = []
+    m = -1
+    
+    for n_ in range(n):
+        temp = list(map(int, sys.stdin.readline().split()))
+        for n__ in range(n):
+            if temp[n__] > m:
+                m = temp[n__]
+        graph.append(temp)
+        
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    
+    def bfs(x,y,rain,graph):
+        if graph[x][y] <= rain:
+            return 0
+        q = deque()
+        q.append((x,y))
+        
+        while(q):
+            x,y = q.popleft()
+            
+            for i in range(4):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                
+                if nx < 0 or ny < 0 or nx > n-1 or ny > n-1:
+                    continue
+                
+                if graph[nx][ny] > rain:
+                    graph[nx][ny] = rain
+                    q.append((nx,ny))
+        return 1
+    
+    last = 0
+    r = 1
+    while(r<m):
+        result = 0
+        g = copy.deepcopy(graph)
+        for i in range(n):
+            for j in range(n):
+                result += bfs(i,j,r,g)
+        
+        if result > last:
+            last = result        
+        r += 1
+        
+    print(last)
+    
+if __name__=='__main__':
+    main()
