1ac7a285cd89b4107ce7d7236e0f19bc24cc8dfb
dyun <zkdlxkxks@nate.com>
2023-05-11 16:17:13 +0900

    update: 그래프탐색 ( 완료 )
    parent 리스트를 만들어서 한번의 bfs로 부모를 저장할 수 있게하고, visited 를 만들어서 양방향 연결에 대응함

--- 11725트리의부모찾기.py
+++ 11725트리의부모찾기.py
@@ -4,27 +4,34 @@ import heapq
 def main():
     n = int(sys.stdin.readline().rstrip())
     graph = [[] for _ in range(n+1)]
+    parent = [[] for _ in range(n+1)]
     root = 1
+    # 부모를 건너지 않고는 자식을 찾아갈 수 없기 때문에 shortest 배열이 필요가 없다.
     for _ in range(n-1):
         a,b = map(int, sys.stdin.readline().split())
         graph[a].append(b)
         graph[b].append(a)
     
-    def bfs(start, end): # 부모를 건너지 않고는 자식을 찾아갈 수 없기 때문에 shortest 배열이 필요가 없다.
+    visited = [False for _ in range(n+1)]
+    
+    def bfs(start): 
         q = []
         heapq.heappush(q, (0,start))
         
         while(q):
             dist, cur = heapq.heappop(q)
+            visited[cur] = True
             
             for g in graph[cur]:
-                if g == end:
-                    return cur
-                heapq.heappush(q, (dist+1, g))
-                    
+                if visited[g] == False:
+                    parent[g] = cur
+                    heapq.heappush(q, (dist+1, g))
+    
+    bfs(root)
     
     for i in range(2,n+1):
-        print(bfs(root, i))
+        print(parent[i])    
+    
     
 if __name__=='__main__':
     main()
