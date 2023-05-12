19f31337f5318acd92ba1afc0ef78e3dcf235073
dyun <zkdlxkxks@nate.com>
2023-05-12 16:13:30 +0900

    update: 그래프 탐색 ( 진행중 )
    record를 재귀마다 copy하는 방법에서 -> 하나의 record로 관리하면서 재귀 들어가기 전에 1로 만들어주고, 들어간 뒤 나올 때 다시 0으로 만들어주는 방법 활용 but 시간초과

--- 1987알파벳.py
+++ 1987알파벳.py
@@ -20,13 +20,12 @@ def main():
     for i in ascii_uppercase:
         record[i] = 0 #record를 한번에 접근할 수 있도록 dict생성
         
+    record[graph[0][0]] = 1
     
-    def dfs(cur,cnt,rrecord): # dfs로 끝까지 가본 뒤 백트래킹으로 돌아올 수 있게 함.
+    def dfs(x,y,cnt,record): # dfs로 끝까지 가본 뒤 백트래킹으로 돌아올 수 있게 함.
         global result
-        x,y = cur
-        sign = graph[x][y]
-        cur_record = rrecord.copy() # record가 다른 깊이에서 변형되지 않게 함. (1차원 dict deepcopy)
-        cur_record[sign] = 1 #visited가 아닌 record로 같은 문자가 있었는지 관리.
+        # cur_record = rrecord.copy() # record가 다른 깊이에서 변형되지 않게 함. (1차원 dict deepcopy)
+        # record를 계속 복사해주는 것은 매우 불필요 및 시간이 많이 걸린다. 따라서 하나의 record로 관리할 수 있게 변경
         if cnt > result:
             result = cnt
         for i in range(4):
@@ -35,12 +34,15 @@ def main():
             
             if nx < 0 or ny < 0 or nx > r-1 or ny > c-1:
                 continue
+            
             n_sign = graph[nx][ny]
-            if cur_record[n_sign] == 0:
-                dfs((nx,ny),cnt+1,cur_record)
+            if record[n_sign] == 0:
+                record[n_sign] = 1
+                dfs(nx,ny,cnt+1,record)
+                record[n_sign] = 0 #다시 뒤로 돌아온다는 것은 dfs를 빠져나왔다는 뜻이니까 그때마다 넣어준 친구를 0으로 초기화
                 
     
-    dfs((0,0),1,record) # 시작 칸도 포함하니 1부터 시작
+    dfs(0,0,1,record) # 시작 칸도 포함하니 1부터 시작
     print(result)
     
 if __name__=='__main__':
