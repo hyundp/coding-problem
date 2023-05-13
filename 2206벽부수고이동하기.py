2b0c94a230b23e8da8c4ec0341c3ad9a8d6a8822
dyun <zkdlxkxks@nate.com>
2023-05-13 17:28:30 +0900

    update: 그래프 탐색 ( 진행중 )
    시간초과
    shortest를 bfs마다 초기화해주는 것에서 시간초과 발생했다고 생각함

--- 2206벽부수고이동하기.py
+++ 2206벽부수고이동하기.py
@@ -5,8 +5,6 @@ def main():
     
     
     n,m = map(int, sys.stdin.readline().split())
-    shortest = [[1e6 for _ in range(m)]for _ in range(n)]
-    shortest[0][0] = 0
     graph = []
     start = []
     for k in range(n):
@@ -22,6 +20,8 @@ def main():
         heapq.heappush(q, (0, 0,0)) # heaq로 좌표 넣을 때는 그냥 이렇게 풀어 써도 됨.
         dx = [-1,1,0,0]
         dy = [0,0,-1,1]
+        shortest = [[1e6 for _ in range(m)]for _ in range(n)]
+        shortest[0][0] = 0
         
         while(q):
             dist,x,y = heapq.heappop(q)
@@ -29,7 +29,7 @@ def main():
                 continue
             
             if (x,y) == (n-1,m-1):
-                return
+                return shortest[n-1][m-1]
             
             for i in range(4):
                 nx = x + dx[i]
@@ -41,23 +41,27 @@ def main():
                 if graph[nx][ny] == 0:
                     if dist+1 < shortest[nx][ny]:
                         shortest[nx][ny] = dist+1
-                        heapq.heappush(q, (dist+1,nx,ny))
-        
-    
-    
+                        heapq.heappush(q, (dist+1,nx,ny))                
+        return shortest[n-1][m-1]
+
+    result = 1e6
     if start == []:
-        bfs()
+        short = bfs()
+        if short < result:
+            result = short
     else:        
         for s in start:
-            graph[s[0]][s[1]] = 0
-            bfs()
+            graph[s[0]][s[1]] = 0 
+            short = bfs()           
+            if short != 1e6:
+                if short < result:
+                    result = short
             graph[s[0]][s[1]] = 1
                 
-    
-    if shortest[n-1][m-1] == 1e6:
+    if result == 1e6:
         print(-1)
     else:
-        print(shortest[n-1][m-1]+1)
+        print(result+1)
 
 
     
