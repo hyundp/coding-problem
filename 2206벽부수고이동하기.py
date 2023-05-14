68ecdff5375b1d68fa14d1c0360206e3faf64fe6
dyun <zkdlxkxks@nate.com>
2023-05-14 20:45:53 +0900

    update: 그래프 탐색 ( 완료 )
    1. 모든 벽을 다 살펴보며 bfs를 반복하면 (n*m)^2번 반복하게 되어 시간이 초과된다
    2. 그렇다면 bfs속에서 벽을 없애가며 벽을 부순 친구들은 그들끼리, 안부순 친구들은 그들끼리 bfs를 돌려보면 된다.
    3. shortest를 3차원으로 만들고, 0번째에는 안부순 친구들의, 1번째에는 부순 친구들의 최단거리를 저장한다.
    4. 벽을 만나면 부수고 그들만의 리그로 보낸다. 만나지 않으면 0들의 리그에서 계속 진행한다. 이는 벽을 부순 친구의 업데이트로 인해 벽을 부수지 않은 친구들의 길이 방해받을 수 있기 때문이다.
    5. 벽을 부쉈든, 부수지 않았든 n,m에 도착하면 그때의 거리를 반환한다.

--- 2206벽부수고이동하기.py
+++ 2206벽부수고이동하기.py
@@ -14,24 +14,24 @@ def main():
         
         graph.append(temp)
 
-    shortest = [[[1e6 for _ in range(m)]for _ in range(n)] for _ in range(len(start)+1)]
-    
-    
-    def bfs(s_idx):
+    shortest = [[[1e6 for _ in range(m)]for _ in range(n)] for _ in range(2)]
+
+# 모든 벽을 다 돌면서 하나 씩 없애는 방법 : O((n*m)^2) -> 시간 초과 발생
+# bfs 속에서 벽을 없애가며 확인해 보는 방법 : O(2*n*m)
+# -> 벽을 안 부순 친구를 처리해주는 n*m matrix, 벽을 부순 친구를 처리해주는 n*m matrix가 존재하며, 벽을 부순 친구들끼리도 역시 shortest경쟁을 하기 때문에 서로 같은 matrix에 존재해야 한다.
+
+    def bfs():
         q = []
-        heapq.heappush(q, (0, 0,0)) # heapq로 좌표 넣을 때는 그냥 이렇게 풀어 써도 됨.
+        heapq.heappush(q, (0, 0,0, 0)) # heapq로 좌표 넣을 때는 그냥 이렇게 풀어 써도 됨.
         dx = [-1,1,0,0]
         dy = [0,0,-1,1]
 
-        shortest[s_idx][0][0] = 0
+        shortest[0][0][0] = 0
             
         while(q):
-            dist,x,y = heapq.heappop(q)
-            if dist > shortest[s_idx][x][y]:
-                continue
-            
+            dist,x,y,wall = heapq.heappop(q)
             if (x,y) == (n-1,m-1):
-                return shortest[s_idx][n-1][m-1]
+                return dist
             
             for i in range(4):
                 nx = x + dx[i]
@@ -41,24 +41,18 @@ def main():
                     continue
                 
                 if graph[nx][ny] == 0:
-                    if dist+1 < shortest[s_idx][nx][ny]:
-                        shortest[s_idx][nx][ny] = dist+1
-                        heapq.heappush(q, (dist+1,nx,ny))
+                    if dist+1 < shortest[wall][nx][ny]:
+                        shortest[wall][nx][ny] = dist+1
+                        heapq.heappush(q, (dist+1,nx,ny,wall))
+                else:
+                    if wall == 0:
+                        if dist+1 < shortest[1][nx][ny]:
+                            shortest[1][nx][ny] = dist+1
+                            heapq.heappush(q, (dist+1,nx,ny,1))
+        return min(shortest[0][n-1][m-1],shortest[1][n-1][m-1])
+                        
 
-    result = 1e6
-    if start == []:
-        bfs(0)
-        if shortest[0][n-1][m-1] < result:
-            result = shortest[0][n-1][m-1]
-    else:        
-        for s in range(len(start)):
-            graph[start[s][0]][start[s][1]] = 0 
-            bfs(s)
-            short = shortest[s][n-1][m-1]         
-            if short != 1e6:
-                if short < result:
-                    result = short
-            graph[start[s][0]][start[s][1]] = 1
+    result = bfs()
                 
     if result == 1e6:
         print(-1)
