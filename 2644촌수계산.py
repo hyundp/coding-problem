59b782f55ccdf2f6ae3c321ae2426f4dc0002920
dyun <zkdlxkxks@nate.com>
2023-05-18 13:57:41 +0900

    update: 그래프탐색 (완료)
    visited, 자식노드 탐색 추가

--- 2644촌수계산.py
+++ 2644촌수계산.py
@@ -1,7 +1,6 @@
 import sys
 
 sys.setrecursionlimit(10**6)
-cnt = 0
 flag = False
 
 def main(): # O(N)
@@ -11,13 +10,15 @@ def main(): # O(N)
     m = int(sys.stdin.readline().rstrip())
     parent = [0 for _ in range(n+1)]
     child = [[] for _ in range(n+1)]
+    visited = [False for _ in range(n+1)] # dfs할땐 visited 필요.
     for _ in range(m):
         a,b = map(int, sys.stdin.readline().split())
         child[a].append(b)
         parent[b] = a
-    def dfs(x):
-        global cnt
+    def dfs(x,cnt):
         global flag
+        
+        visited[x] = True
         
         if x == end:
             print(cnt)
@@ -29,13 +30,16 @@ def main(): # O(N)
             flag = True
             return
         
-        if parent[x] != 0:
-            cnt+=1
-            dfs(parent[x])
+        if parent[x] != 0 and visited[parent[x]] == False: # 위로 가는 경우
+            dfs(parent[x],cnt+1)
+        
+        if child[x] != []: # 아래로 가는 경우
+            for c in child[x]:
+                if visited[c] == False:
+                    dfs(c,cnt+1)
             
         
-    
-    dfs(start)
+    dfs(start,0)
     if flag == False:
         print(-1)
     
