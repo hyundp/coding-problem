62cdacd2f466c64e911aba3a7a65ed515d940640
dyun <zkdlxkxks@nate.com>
2023-05-21 23:55:42 +0900

    update: 그래프탐색 ( 진행중 )
    현재 visited로 갔던 곳을 +1만 해버리면, 다른 경우의 수들이 없어지기에, 그 길로 가면 생길 수 있는 경우를 모두 더해줄 수 있도록 코드 작성 중

--- 1520내리막길.py
+++ 1520내리막길.py
@@ -6,11 +6,9 @@ sys.setrecursionlimit(10**6)
 cnt = 0
 m,n = map(int, sys.stdin.readline().split())
 visited = [[0 for _ in range(n)]for _ in range(m)]
-flag = False
 
 def main():
     graph = []
-    
     for _ in range(m):
         graph.append(list(map(int, sys.stdin.readline().split())))
         
@@ -20,34 +18,30 @@ def main():
     
     def dfs(x,y):
         global cnt
-        global flag
         global visited
+        
+        print(x,y, visited[x][y])
+        print(visited)
+        
+        if visited[x][y] != 0:
+            cnt += visited[x][y]
+            return
         
         if (x,y) == (m-1,n-1):
            cnt+=1
-           return 1
+           return
         
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            
             if nx < 0 or ny < 0 or nx > m-1 or ny > n-1:
                 continue
-            
             if graph[nx][ny] < graph[x][y]:
-                if visited[nx][ny] == 1:
-                    cnt+=1
-                    return 1
-                else:
-                    flag = False
-                    r = dfs(nx,ny)
-                    if r == 1:
-                        flag = True
-                    if flag == True:
-                        visited[nx][ny] = 1               
-        return 0
+                dfs(nx,ny)
+        visited[x][y] += cnt             
+        return
     dfs(0,0)
-    print(cnt)
+    print(visited[0][0])
     
     
 if __name__=='__main__':
