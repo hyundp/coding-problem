52f5266fffff44ae1f395f5d89034d674d2aaa00
dyun <zkdlxkxks@nate.com>
2023-05-23 23:54:23 +0900

    update: 그래프 탐색(진행중)
    visited를 통해 방문했던 점(그 점으로 갔을 때 몇개의 길이 나오는지 정해진 점)은 visited에 그 점으로 가면 몇개의 길이 있는지 저장하고, 다음에 그 점을 방문하면 더이상 탐색하지 않고 길의 개수를 더해주기만 하는 방식으로 시간복잡도를 줄인다 -> 시간초과

--- 1520내리막길.py
+++ 1520내리막길.py
@@ -13,34 +13,43 @@ def main():
         graph.append(list(map(int, sys.stdin.readline().split())))
         
     dx = [-1,1,0,0]
-    dy = [0,0,-1,1]    
+    dy = [0,0,-1,1]
+    
+    if m == 1 and n == 1:
+        print(1)
+        return
+    
+    if graph[m-2][n-1] > graph[m-1][n-1]:
+        visited[m-2][n-1] = 1
+    if graph[m-1][n-2] > graph[m-1][n-1]:
+        visited[m-1][n-2] = 1
     
     
     def dfs(x,y):
         global cnt
         global visited
         
-        print(x,y, visited[x][y])
-        print(visited)
-        
-        if visited[x][y] != 0:
-            cnt += visited[x][y]
-            return
-        
-        if (x,y) == (m-1,n-1):
-           cnt+=1
-           return
+        # print(x,y)
+        # print(visited)
         
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
+            
             if nx < 0 or ny < 0 or nx > m-1 or ny > n-1:
                 continue
+        
             if graph[nx][ny] < graph[x][y]:
+                if visited[nx][ny] != 0:
+                    visited[x][y] += visited[nx][ny]
+                    continue
                 dfs(nx,ny)
-        visited[x][y] += cnt             
-        return
-    dfs(0,0)
+                visited[x][y] += visited[nx][ny]
+    
+    if visited[m-2][n-1] == 1 or visited[m-1][n-2] == 1:
+        dfs(0,0)             
+    
+    # print(visited)
     print(visited[0][0])
     
     
